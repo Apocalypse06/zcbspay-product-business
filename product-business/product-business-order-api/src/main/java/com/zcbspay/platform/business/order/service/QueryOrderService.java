463a55b43c9b56dbd41689ac10db2af0e505fe02
lianhai <lianhai@192.168.11.1>
2017-03-17 13:00:41 +0800

    完成订单查询 —— 张连海

--- product-business/product-business-order-api/src/main/java/com/zcbspay/platform/business/order/service/QueryOrderService.java
+++ product-business/product-business-order-api/src/main/java/com/zcbspay/platform/business/order/service/QueryOrderService.java
@@ -1,33 +1,85 @@
 package com.zcbspay.platform.business.order.service;
 
-import com.zcbspay.platform.business.exception.BusinessOrderException;
-import com.zcbspay.platform.business.order.bean.OrderResultBean;
+import com.zcbspay.platform.business.exception.QueryOrderException;
+import com.zcbspay.platform.business.order.bean.ResultBean;
+
 /***
  * 查询订单相关
- * @author liusm
+ * 
+ * @author lianhai
  *
  */
 public interface QueryOrderService {
-	/****
-	 * 根据tn查询订单
+	/**
+	 * 交易订单查询方法
+	 * 
+	 * @param merchNo
+	 *            商户号
+	 * @param orderId
+	 *            订单号
+	 * @return 订单结果bean
+	 */
+	public ResultBean queryOrder(String merchNo, String orderId) throws QueryOrderException;
+
+	/**
+	 * 交易订单查询方法
+	 * 
+	 * @param tn
+	 *            受理订单号
+	 * @return
+	 */
+	public ResultBean queryOrderByTN(String tn) throws QueryOrderException;
+
+	/**
+	 * 
+	 * @param merchNo
+	 * @param orderId
+	 * @return
+	 * @throws QueryOrderException
+	 */
+	public ResultBean queryInsteadPayOrder(String merchNo, String orderId) throws QueryOrderException;
+
+	/**
+	 * 查询实时集中代收订单
+	 * 
 	 * @param tn
 	 * @return
+	 * @throws QueryOrderException
 	 */
-	public OrderResultBean queryOrder(String tn ) throws BusinessOrderException;
-	/***
-	 * 根据商户和授理订单号查询订单
-	 * @param merchId
-	 * @param accOrderNo
-	 * @return
-	 */
-	public OrderResultBean queryOrder(String merchId, String accOrderNo) throws BusinessOrderException;
-	/***
+	public ResultBean queryConcentrateCollectionOrder(String tn) throws QueryOrderException;
+
+	/**
+	 * 查询实时集中代付订单
 	 * 
-	 * @param merchId
-	 * @param accOrderNo
+	 * @param tn
 	 * @return
-	 * @throws BusinessOrderException
+	 * @throws QueryOrderException
 	 */
-	public OrderResultBean queryInstPayOrder(String merchId, String accOrderNo)throws BusinessOrderException;
-	
+	public ResultBean queryConcentratePaymentOrder(String tn) throws QueryOrderException;
+
+	/**
+	 * 查询集中代收批次信息
+	 * 
+	 * @param merchNo
+	 *            商户号
+	 * @param batchNo
+	 *            批次号
+	 * @param txnDate
+	 *            交易日期
+	 * @return
+	 */
+	public ResultBean queryConcentrateCollectionBatch(String merchNo, String batchNo, String txnDate);
+
+	/**
+	 * 查询集中代付批次信息
+	 * 
+	 * @param merchNo
+	 *            商户号
+	 * @param batchNo
+	 *            批次号
+	 * @param txnDate
+	 *            交易日期
+	 * @return
+	 */
+	public ResultBean queryConcentratePaymentBatch(String merchNo, String batchNo, String txnDate);
 }
---
+++ product-business/product-business-order/src/main/java/com/zcbspay/platform/business/order/service/impl/QueryOrderServiceImpl.java
@@ -0,0 +1,111 @@
+package com.zcbspay.platform.business.order.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zcbspay.platform.business.commons.utils.BeanCopyUtil;
+import com.zcbspay.platform.business.exception.QueryOrderException;
+import com.zcbspay.platform.business.order.bean.BatchResultBean;
+import com.zcbspay.platform.business.order.bean.OrderResultBean;
+import com.zcbspay.platform.business.order.bean.ResultBean;
+import com.zcbspay.platform.business.order.service.QueryOrderService;
+
+@Service
+public class QueryOrderServiceImpl implements QueryOrderService {
+
+	@Autowired
+	private com.zcbspay.platform.support.order.query.query.service.QueryOrderService queryOrderService;
+
+	@Override
+	public ResultBean queryOrder(String merchNo, String orderId) throws QueryOrderException {
+		OrderResultBean orderResultBean;
+		try {
+			orderResultBean = BeanCopyUtil.copyBean(OrderResultBean.class,
+					queryOrderService.queryOrder(merchNo, orderId));
+		} catch (com.zcbspay.platform.support.order.query.exception.QueryOrderException e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(orderResultBean);
+	}
+
+	@Override
+	public ResultBean queryOrderByTN(String tn) throws QueryOrderException {
+		OrderResultBean orderResultBean;
+		
+		try {
+			orderResultBean = BeanCopyUtil.copyBean(OrderResultBean.class,
+					queryOrderService.queryOrderByTN(tn));
+		} catch (com.zcbspay.platform.support.order.query.exception.QueryOrderException e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(orderResultBean);
+	}
+
+	@Override
+	public ResultBean queryInsteadPayOrder(String merchNo, String orderId) throws QueryOrderException {
+		OrderResultBean orderResultBean;
+		try {
+			orderResultBean = BeanCopyUtil.copyBean(OrderResultBean.class,
+					queryOrderService.queryInsteadPayOrder(merchNo, orderId));
+		} catch (com.zcbspay.platform.support.order.query.exception.QueryOrderException e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(orderResultBean);
+	}
+
+	@Override
+	public ResultBean queryConcentrateCollectionOrder(String tn) throws QueryOrderException {
+		OrderResultBean orderResultBean;
+		try {
+			orderResultBean = BeanCopyUtil.copyBean(OrderResultBean.class,
+					queryOrderService.queryConcentrateCollectionOrder(tn));
+		} catch (com.zcbspay.platform.support.order.query.exception.QueryOrderException e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(orderResultBean);
+	}
+
+	@Override
+	public ResultBean queryConcentratePaymentOrder(String tn) throws QueryOrderException {
+		OrderResultBean orderResultBean;
+		try {
+			orderResultBean = BeanCopyUtil.copyBean(OrderResultBean.class,
+					queryOrderService.queryConcentratePaymentOrder(tn));
+		} catch (com.zcbspay.platform.support.order.query.exception.QueryOrderException e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(orderResultBean);
+	}
+
+	@Override
+	public ResultBean queryConcentrateCollectionBatch(String merchNo, String batchNo, String txnDate) {
+		BatchResultBean batchResultBean;
+		try {
+			batchResultBean = BeanCopyUtil.copyBean(BatchResultBean.class,
+					queryOrderService.queryConcentrateCollectionBatch(merchNo, batchNo, txnDate));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(batchResultBean);
+	}
+
+	@Override
+	public ResultBean queryConcentratePaymentBatch(String merchNo, String batchNo, String txnDate) {
+		BatchResultBean batchResultBean;
+		try {
+			batchResultBean = BeanCopyUtil.copyBean(BatchResultBean.class,
+					queryOrderService.queryConcentratePaymentBatch(merchNo, batchNo, txnDate));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResultBean("BO00012", e.getMessage());
+		}
+		return new ResultBean(batchResultBean);
+	}
+
+}
